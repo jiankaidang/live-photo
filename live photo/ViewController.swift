7f0a5f55684c35a63e84b29e922bfda6337ae7d0
dangj <dangj@yahoo-inc.com>
2016-01-11 00:27:23 -0800

    live photo

---
+++ live photo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  live photo
+//
+//  Created by Jiankai Dang on 1/10/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
